e7f7591144d6829ae337d6343d8b072fe475b706
Ka Wong <kawong2020@u.northwestern.edu>
2020-01-21 16:29:07 -0600

    styled static catalog page with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+
+import "rbx/index.css";
+import {
+  Column,
+  Card,
+  Content,
+  Divider,
+  Image,
+  Title,
+  Button
+} from "rbx";
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  const productGroups = [
+    ...Array(Math.ceil(products.length / 4)).keys()
+  ].map(i => products.slice(4 * i, 4 * i + 4));
+
+  const sizes = ["S", "M", "L", "XL"];
+
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
+      const response = await fetch("./data/products.json");
       const json = await response.json();
       setData(json);
     };
@@ -13,9 +30,45 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <div>
+      {productGroups.map((products, idx) => (
+        <Column.Group key={idx}>
+          {products.map(product => (
+            <Column size={3} key={product.sku}>
+              <Card>
+                <Card.Image>
+                  <Image.Container>
+                    <Image src={`./data/products/${product.sku}_1.jpg`} />
+                  </Image.Container>
+                </Card.Image>
+                <Card.Content align="center">
+                  <Content>
+                    <Title as="p" size={6}>
+                      {product.title}
+                    </Title>
+                    {`Description: ${product.description || "N/A"}`}
+                    <Button.Group
+                      style={{ justifyContent: "center", marginTop: "5px" }}
+                    >
+                      {sizes.map(size => (
+                        <Button key={size}>{size}</Button>
+                      ))}
+                    </Button.Group>
+                    <Divider />
+                    {`$${parseFloat(product.price).toFixed(2)}`}
+                    <Button.Group
+                      style={{ justifyContent: "center", marginTop: "10px" }}
+                    >
+                      <Button color="black">Add to cart</Button>
+                    </Button.Group>
+                  </Content>
+                </Card.Content>
+              </Card>
+            </Column>
+          ))}
+        </Column.Group>
+      ))}
+    </div>
   );
 };
 
